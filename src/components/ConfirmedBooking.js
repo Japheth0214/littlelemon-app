a2d364bd7f5434a36c840f8b5c4c1fb65bf49e45
Japheth0214 <japarcillians@gmail,com>
2023-09-07 13:25:29 +0800

    first commit

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ConfirmedBooking = () => {
+    return(
+        <div className="comfirm">
+            <div>
+            <h1>Booking has been <span>confirmed!</span></h1>
+          <h1><span>Thank you</span> for booking with us!</h1> 
+            </div>
+        </div>
+
+    )
+}
+
+export default ConfirmedBooking;
